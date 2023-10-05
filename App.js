9699b7607dd87449b92d8665e041a27623f5c098
Psalm-Script <139768813+Psalm-Script@users.noreply.github.com>
2023-10-05 11:29:25 +0100

    Add files via upload
    This is the Weather Application that receives JSON weather data from the OpenWeatherMaps.com site. First react Native App , enjoy

---
+++ App.js
@@ -0,0 +1,69 @@
+import React, {useEffect, useState}  from "react"
+import { View,  StyleSheet, ActivityIndicator } from "react-native"
+import {NavigationContainer} from '@react-navigation/native'
+import Tabs from './src/components/Tabs'
+import * as Location from 'expo-location'
+import {WEATHER_API_KEY} from '@env'
+import { useGetWeather } from "./src/hooks/useGetWeather"
+import ErrorItem from "./src/components/ErrorItem"
+
+
+const App = () => {
+  const [loading, error, weather] = useGetWeather()
+
+  if (weather && weather.list && !loading) {
+  return (
+    <NavigationContainer>
+      <Tabs weather = {weather} />
+      </NavigationContainer>
+    )
+  }
+
+  return (
+  <View style= {styles.container}>
+    {error ? (
+      <ErrorItem />
+      ) : (
+      <ActivityIndicator size = {'large'} color={'blue'}/> 
+
+        
+      )}
+  </View>
+)
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    flex: 1
+  }
+});
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const App = () => {
+//  const[loading, error, weather] = useGetWeather()
+//  console.log(loading, error, weather)
+
+//  if (weather && weather.list ) {
+//    return (
+//      <NavigationContainer>
+//        <Tabs weather={weather} />
+//      </NavigationContainer>
+//    ) 
+//  }
+
